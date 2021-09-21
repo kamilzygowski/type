5fa522d136a43262cf221f518a4a62c9c5a25b2c
swedishsailor <kzygowski7@gmail.com>
2021-09-22 00:04:22 +0200

    setInterval set up and drawing main things

--- index.js
+++ index.js
@@ -2,12 +2,56 @@
 exports.__esModule = true;
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-var minBallSize = 40;
+var button = [];
+var minBallSize = 80;
 var maxBallSize = 120;
+var color;
+var player = {
+    x: 150,
+    y: 150,
+    speed: 4
+};
+var pallete = ["#9c88ff", "#0097e6", "#353b48", "#1B1464", "#ED4C67", "#FFC312"];
+var playerImg = new Image;
+playerImg.src = "images/circle.png";
 console.log("canvas width = " + canvas.width + " canvas height = " + canvas.height);
-function randomBall() {
-    ctx.beginPath();
-    ctx.arc(Math.floor(Math.random() * canvas.width + 1), Math.floor(Math.random() * canvas.height + minBallSize + 1), Math.floor(Math.random() * maxBallSize + 1), 0, Math.PI * 2, true);
-    ctx.stroke();
+function ballColor() {
+    color = pallete[Math.floor(Math.random() * pallete.length)];
+    console.log(color);
 }
-randomBall();
+function randomBall() {
+    ballColor();
+    ctx.beginPath();
+    ctx.fillStyle = color;
+    ctx.fill();
+}
+function playerBallArc() {
+    ctx.beginPath();
+    player = ctx.arc(0 + Math.floor(Math.random() * maxBallSize + minBallSize), 0 + Math.floor(Math.random() * maxBallSize + minBallSize), Math.floor(Math.random() * maxBallSize + minBallSize), 0, Math.PI * 2, true);
+    ctx.fillStyle = "pink";
+    ctx.fill();
+}
+function playerBallImg() {
+    ctx.drawImage(playerImg, player.x, player.y);
+}
+playerBallImg();
+//randomBall();
+setInterval(function () {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    playerBallImg();
+    if (button["w"])
+        player.y -= player.speed;
+    if (button["a"])
+        player.x -= player.speed;
+    if (button["s"])
+        player.y += player.speed;
+    if (button["d"])
+        player.x += player.speed;
+}, 1000 / 60);
+window.addEventListener("keydown", function (e) {
+    button[e.key] = 1;
+});
+window.addEventListener("keyup", function (e) {
+    delete button[e.key];
+});
+console.log(player);
--- index.ts
+++ index.ts
@@ -2,17 +2,66 @@ import * as _ from 'loadash';
 
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-let minBallSize: number = 40;
+const button = [];
+let minBallSize: number = 80;
 let maxBallSize: number = 120;
+let color: string;
+let player = {
+    x : 150,
+    y : 150,
+    speed : 4,
+};
+const pallete: string[] = ["#9c88ff", "#0097e6", "#353b48", "#1B1464", "#ED4C67", "#FFC312"];
+const playerImg = new Image;
+playerImg.src = "images/circle.png";
 
 console.log("canvas width = " + canvas.width + " canvas height = " + canvas.height);
 
+function ballColor() {
+    color = pallete[Math.floor(Math.random() * pallete.length)];
+    console.log(color);
+}
+
 function randomBall() {
 
+    ballColor()
     ctx.beginPath();
-    ctx.arc(Math.floor(Math.random()* canvas.width + 1), Math.floor(Math.random()* canvas.height + minBallSize + 1),  Math.floor(Math.random()* maxBallSize + 1), 0, Math.PI * 2, true);
-    ctx.stroke();
+    ctx.fillStyle = color;
+    ctx.fill();
 
 }
 
-randomBall();
+function playerBallArc() {
+    ctx.beginPath();
+    player = ctx.arc(0 + Math.floor(Math.random()* maxBallSize + minBallSize) ,0 + Math.floor(Math.random()* maxBallSize + minBallSize) ,  Math.floor(Math.random()* maxBallSize + minBallSize), 0, Math.PI * 2, true);
+    ctx.fillStyle = "pink";
+    ctx.fill();
+}
+
+function playerBallImg() {
+    ctx.drawImage(playerImg, player.x, player.y);
+}
+
+playerBallImg();
+//randomBall();
+
+setInterval(function() {
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    playerBallImg();
+
+    if (button["w"]) player.y-= player.speed;
+    if (button["a"]) player.x-= player.speed;
+    if (button["s"]) player.y+= player.speed;
+    if (button["d"]) player.x+= player.speed;
+
+}, 1000/60);
+
+window.addEventListener("keydown", function(e){
+    button[e.key] = 1;
+})
+
+window.addEventListener("keyup", function(e){
+    delete button[e.key];
+})
+
+console.log(player);
