1479e40e7ef9ae0d0a249962dfac220c951ea387
swedishsailor <kzygowski7@gmail.com>
2021-09-22 02:26:03 +0200

    basic shooting

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ exports.__esModule = true;
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 var button = [];
+var bullet = {};
+var flyingBullet = [];
+var i = -1;
+var bulletBool;
 var minBallSize = 80;
 var maxBallSize = 120;
 var color;
 var player = {
     x: 150,
     y: 150,
-    speed: 4
+    width: 155,
+    height: 155,
+    speed: 8
 };
 var pallete = ["#9c88ff", "#0097e6", "#353b48", "#1B1464", "#ED4C67", "#FFC312"];
 var playerImg = new Image;
@@ -31,27 +37,58 @@ function playerBallArc() {
     ctx.fillStyle = "pink";
     ctx.fill();
 }
+function spawnBullet() {
+    var shot = {
+        x: player.x,
+        y: player.y
+    };
+    ctx.fillStyle = "red";
+    var x = flyingBullet[i];
+    ctx.drawImage(playerImg, x, bullet.y, 30, 30);
+    bulletBool = true;
+}
+function adder(number) {
+    number++;
+}
 function playerBallImg() {
-    ctx.drawImage(playerImg, player.x, player.y);
+    ctx.drawImage(playerImg, player.x, player.y, player.width, player.height);
 }
 playerBallImg();
 //randomBall();
 setInterval(function () {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    playerBallImg();
-    if (button["w"])
+    ctx.fillStyle = "#1B1464";
+    //ctx.clearRect(0,0,canvas.width, canvas.height); // Background
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    playerBallImg(); // Spawn Player
+    //console.log(player.x, player.y);
+    if (button["w"] && player.y >= 0)
         player.y -= player.speed;
-    if (button["a"])
+    if (button["a"] && player.x >= 0)
         player.x -= player.speed;
-    if (button["s"])
+    if (button["s"] && player.y <= canvas.height - player.height)
         player.y += player.speed;
-    if (button["d"])
+    if (button["d"] && player.x <= canvas.width - player.width)
         player.x += player.speed;
+    /* making shooting animation in rendering */
+    if (bulletBool == true) {
+        spawnBullet();
+        ctx.fillStyle = "red";
+        flyingBullet[i] += 50;
+    }
 }, 1000 / 60);
 window.addEventListener("keydown", function (e) {
     button[e.key] = 1;
+    if (e.code === 'Space') {
+        spawnBullet();
+        bulletBool;
+        bullet.x = player.x;
+        bullet.y = player.y;
+        i++;
+        flyingBullet.splice(i, 0, player.x);
+        console.log(flyingBullet[i]);
+    }
 });
 window.addEventListener("keyup", function (e) {
     delete button[e.key];
 });
-console.log(player);
+console.log(canvas.x);
--- index.ts
+++ index.ts
@@ -3,14 +3,25 @@ import * as _ from 'loadash';
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const button = [];
+let bullet = {};
+let flyingBullet = [];
+let i = -1;
+let bulletBool;
 let minBallSize: number = 80;
 let maxBallSize: number = 120;
 let color: string;
 let player = {
     x : 150,
     y : 150,
-    speed : 4,
+    width : 155,
+    height : 155,
+    speed : 8,
 };
+interface Bullet {
+    x: number,
+    y: number,
+    key: number?,
+}
 const pallete: string[] = ["#9c88ff", "#0097e6", "#353b48", "#1B1464", "#ED4C67", "#FFC312"];
 const playerImg = new Image;
 playerImg.src = "images/circle.png";
@@ -38,30 +49,69 @@ function playerBallArc() {
     ctx.fill();
 }
 
+function spawnBullet() {
+    const shot: Bullet = {
+        x: player.x,
+        y: player.y,
+    };
+    ctx.fillStyle = "red";
+    const x = flyingBullet[i];
+    ctx.drawImage(playerImg, x, bullet.y, 30, 30); 
+    bulletBool = true;
+}
+
+function adder(number) {
+    number++;
+}
+
 function playerBallImg() {
-    ctx.drawImage(playerImg, player.x, player.y);
+    ctx.drawImage(playerImg, player.x, player.y, player.width, player.height);
 }
 
 playerBallImg();
 //randomBall();
 
 setInterval(function() {
-    ctx.clearRect(0,0,canvas.width, canvas.height);
-    playerBallImg();
+    ctx.fillStyle = "#1B1464";
+    //ctx.clearRect(0,0,canvas.width, canvas.height); // Background
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    playerBallImg(); // Spawn Player
+    //console.log(player.x, player.y);
 
-    if (button["w"]) player.y-= player.speed;
-    if (button["a"]) player.x-= player.speed;
-    if (button["s"]) player.y+= player.speed;
-    if (button["d"]) player.x+= player.speed;
+    if (button["w"] && player.y >= 0) player.y-= player.speed;
+    if (button["a"] && player.x >= 0) player.x-= player.speed;
+    if (button["s"] && player.y <= canvas.height - player.height) player.y+= player.speed;
+    if (button["d"] && player.x <= canvas.width - player.width) player.x+= player.speed;
+
+    /* making shooting animation in rendering */
+    if (bulletBool == true) {
+        spawnBullet();
+        ctx.fillStyle = "red";
+        flyingBullet[i] += 50;
+        
+
+
+    }
 
 }, 1000/60);
 
 window.addEventListener("keydown", function(e){
     button[e.key] = 1;
+
+    if (e.code === 'Space') {
+        spawnBullet();
+        bulletBool;
+        bullet.x = player.x;
+        bullet.y = player.y;
+        i++;
+        flyingBullet.splice(i, 0, player.x);
+        console.log(flyingBullet[i]);
+      }
 })
 
 window.addEventListener("keyup", function(e){
     delete button[e.key];
 })
 
-console.log(player);
+
+console.log(canvas.x);
