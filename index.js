322b022535014e0ae36de4ad2072556f67708e20
swedishsailor <kzygowski7@gmail.com>
2021-09-23 18:45:56 +0200

    collision and player death added

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 var button = [];
 var flyingBullet = [];
-var scrollingSpeed = 0; // It is ALWAYS added to an object that is not moving with background
+var scrollingSpeed = 2; // It is ALWAYS added to an object that is not moving with background
 var counter = 0;
 var color;
 var gameState = false;
+var expframeTime = 0;
+var expthisFrame = 0;
+var playerIsDead = false;
+var canIMove = true;
 var gameInterval;
 var player = {
     x: 75,
@@ -16,49 +20,57 @@ var player = {
     height: 128,
     speed: 8,
     thisFrame: 0,
-    frameTime: 0
+    frameTime: 0,
+    radius: 128
 };
 var floatingEnemy = [{
-        x: 1222,
+        x: 1222 - scrollingSpeed,
         y: 555,
         width: 128,
-        height: 128
+        height: 128,
+        radius: 0 - 60
     },
     {
-        x: 1666,
+        x: 1666 - scrollingSpeed,
         y: 366,
         width: 128,
-        height: 128
+        height: 128,
+        radius: 0 - 60
     },
     {
-        x: 855,
+        x: 855 - scrollingSpeed,
         y: 129,
         width: 128,
-        height: 128
+        height: 128,
+        radius: 0 - 60
     },
     {
-        x: 2050,
+        x: 2050 - scrollingSpeed,
         y: 566,
         width: 128,
-        height: 128
+        height: 128,
+        radius: 0 - 60
     },
     {
-        x: 2311,
+        x: 2311 - scrollingSpeed,
         y: 799,
         width: 128,
-        height: 128
+        height: 128,
+        radius: 0 - 60
     },
     {
-        x: 522,
+        x: 522 - scrollingSpeed,
         y: 611,
         width: 128,
-        height: 128
+        height: 128,
+        radius: 0 - 60
     },
     {
-        x: 2811,
+        x: 2811 - scrollingSpeed,
         y: 419,
         width: 128,
-        height: 128
+        height: 128,
+        radius: 0 - 60
     },
 ];
 var pallete = ["#9c88ff", "#0097e6", "#353b48", "#1B1464", "#ED4C67", "#FFC312"];
@@ -73,11 +85,15 @@ var floatingEnemyImg = new Image;
 floatingEnemyImg.src = "images/enemy.png";
 var bombImg = new Image;
 bombImg.src = "images/bomb.png";
+var playerDeathImg = new Image;
+playerDeathImg.src = "images/explosion.png";
 /* SELECTORS */
 var playButton = document.querySelector('#playBtn');
 var menuDiv = document.querySelector('.menu');
 var pauseDiv = document.querySelector('#pauseScreen');
 var mainMenuButton = document.querySelector('.mainMenuButton');
+var mainMenuButton2 = document.querySelector('.mainMenuButton2');
+var restartDiv = document.querySelector('#restartScreen');
 ctx.canvas.width = window.innerWidth * 4; // HERE is *2 because background is 3840px width, not 1920px
 ctx.canvas.height = window.innerHeight - 20;
 console.log("canvas width = " + canvas.width + " canvas height = " + canvas.height);
@@ -100,6 +116,9 @@ function floatingEnemyLogic() {
         floatingEnemy[i].y -= Math.floor(Math.random() * 14 + 1);
     }
 }
+function reloadPage() {
+    window.location.reload();
+}
 function playerHitbox() {
     ctx.beginPath();
     ctx.arc(player.x, player.y, player.width / 2, 0, Math.PI * 2, true);
@@ -112,6 +131,29 @@ function drawPlayer() {
     player.thisFrame = Math.round(player.frameTime / 10);
     ctx.drawImage(playerImg, player.width * player.thisFrame, 0, player.width, player.height, player.x, player.y, player.width, player.height);
 }
+function playerDies() {
+    expframeTime += 6.9;
+    expframeTime = expframeTime % 710;
+    expthisFrame = Math.round(expframeTime / 10);
+    var deadX = player.x;
+    var deadY = player.y;
+    playerImg.src = "images/empty.png";
+    canIMove = false;
+    ctx.drawImage(playerDeathImg, 100 * expthisFrame, 0, 100, 100, deadX - 18 * 2, deadY - 18 * 2, 100 * 2, 100 * 2);
+    setTimeout(function () {
+        clearInterval(gameInterval);
+        restartDiv.classList.remove('hide');
+    }, 1600); // Cancel set interval adter explosion animation
+}
+function collision(object1, object2) {
+    var dx = object1.x - object2.x;
+    var dy = object1.y - object2.y;
+    var distance = Math.sqrt(dx * dx + dy * dy);
+    if (distance < object1.radius + object2.radius) {
+        playerIsDead = true; // When collied, player is dead
+        //clearInterval(gameInterval);
+    }
+}
 /* Main Functions init BELOW */
 // Clicking button PLAY  in menu
 playButton.addEventListener('click', function () {
@@ -120,7 +162,7 @@ playButton.addEventListener('click', function () {
     Game();
     console.log('gamestate = true');
 });
-//randomBall();
+/* START and body of the game itself */
 function Game() {
     if (gameState == true) {
         clearInterval(gameInterval);
@@ -137,18 +179,21 @@ function Game() {
             //playerHitbox();
             // Drawing floating blue enemy
             for (var x = 0; x < floatingEnemy.length; x++) {
-                ctx.drawImage(floatingEnemyImg, floatingEnemy[x].x + scrollingSpeed, floatingEnemy[x].y, floatingEnemy[x].width, floatingEnemy[x].height);
+                ctx.drawImage(floatingEnemyImg, floatingEnemy[x].x, floatingEnemy[x].y, floatingEnemy[x].width, floatingEnemy[x].height);
+                collision(player, floatingEnemy[x]); // COLLISION player with floating enemy
             }
             floatingEnemyLogic(); // Apply a logic to the enemy
-            ctx.drawImage(bombImg, 755 + scrollingSpeed, 466, 64, 64); // drawing random bomb
-            if ((button["w"] || button["W"]) && player.y >= 0)
-                player.y -= player.speed;
-            if ((button["a"] || button["A"]) && player.x >= 0)
-                player.x -= player.speed;
-            if ((button["s"] || button["S"]) && player.y <= canvas.height - player.height)
-                player.y += player.speed;
-            if ((button["d"] || button["D"]) && player.x <= canvas.width - player.width)
-                player.x += player.speed;
+            //ctx.drawImage(bombImg, 755 + scrollingSpeed, 466, 64, 64); // drawing random bomb ! IMPORTANT
+            if (canIMove == true) {
+                if ((button["w"] || button["W"]) && player.y >= 0)
+                    player.y -= player.speed;
+                if ((button["a"] || button["A"]) && player.x >= 0)
+                    player.x -= player.speed;
+                if ((button["s"] || button["S"]) && player.y <= canvas.height - player.height)
+                    player.y += player.speed;
+                if ((button["d"] || button["D"]) && player.x <= canvas.width - player.width)
+                    player.x += player.speed;
+            }
             if (button["Escape"]) { // On ESCAPE click go back to menu
                 clearInterval(gameInterval);
                 pauseDiv.classList.remove('hide');
@@ -161,6 +206,10 @@ function Game() {
                 ctx.drawImage(bulletImg, bullets.x - bullets.width / 2, bullets.y - bullets.height / 2, bullets.width, bullets.height);
                 bullets.x += 35;
             }
+            if (playerIsDead == true) {
+                playerDies();
+            }
+            /* COLLISIONS */
         }, 1000 / 60);
     }
 }
@@ -194,11 +243,18 @@ window.addEventListener("click", function (e) {
 window.addEventListener("keyup", function (e) {
     delete button[e.key];
 });
-// When paused, on press main menu display menu and turn the game from hte start
+// When paused, on press main menu display menu and turn the game from hte start IT'S FROM GAME PAUSE
 mainMenuButton.addEventListener('click', function () {
-    /*clearInterval(gameInterval);
+    clearInterval(gameInterval);
     pauseDiv.classList.add('hide');
-    menuDiv.classList.remove('hide');*/
-    window.location.reload(); // Reload the page after clicking on mainButton, it's a little hack >:)
+    menuDiv.classList.remove('hide');
+    reloadPage();
+});
+// On game over press main menu to refresh the game IT'S FROM GAME OVER SCREEN!
+mainMenuButton2.addEventListener('click', function () {
+    clearInterval(gameInterval);
+    restartDiv.classList.add('hide');
+    menuDiv.classList.remove('hide');
+    reloadPage();
 });
 console.log(canvas.x);
--- index.ts
+++ index.ts
@@ -4,61 +4,73 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const button = [];
 let flyingBullet = [];
-let scrollingSpeed = 0; // It is ALWAYS added to an object that is not moving with background
+let scrollingSpeed = 2; // It is ALWAYS added to an object that is not moving with background
 let counter = 0;
 let color: string;
 let gameState: bool = false;
+let expframeTime = 0;
+let expthisFrame = 0;
+let playerIsDead: bool = false;
+let canIMove: bool = true;
 let gameInterval;
 let player = {
-    x : 75,
-    y : 445,
-    width : 128,
-    height : 128,
-    speed : 8,
+    x: 75,
+    y: 445,
+    width: 128,
+    height: 128,
+    speed: 8,
     thisFrame: 0,
     frameTime: 0,
+    radius: 128,
 };
-let floatingEnemy =[{
-    x : 1222,
-    y : 555,
-    width : 128,
-    height : 128,
+let floatingEnemy = [{
+    x: 1222 - scrollingSpeed,
+    y: 555,
+    width: 128,
+    height: 128,
+    radius: 0 - 60,
 },
 {
-    x : 1666,
-    y : 366,
-    width : 128,
-    height : 128,
+    x: 1666 - scrollingSpeed,
+    y: 366,
+    width: 128,
+    height: 128,
+    radius: 0 - 60,
 },
 {
-    x : 855,
-    y : 129,
-    width : 128,
-    height : 128,
+    x: 855 - scrollingSpeed,
+    y: 129,
+    width: 128,
+    height: 128,
+    radius: 0 - 60,
 },
 {
-    x : 2050,
-    y : 566,
-    width : 128,
-    height : 128,
+    x: 2050 - scrollingSpeed,
+    y: 566,
+    width: 128,
+    height: 128,
+    radius: 0 - 60,
 },
 {
-    x : 2311,
-    y : 799,
-    width : 128,
-    height : 128,
+    x: 2311 - scrollingSpeed,
+    y: 799,
+    width: 128,
+    height: 128,
+    radius: 0 - 60,
 },
 {
-    x : 522,
-    y : 611,
-    width : 128,
-    height : 128,
+    x: 522 - scrollingSpeed,
+    y: 611,
+    width: 128,
+    height: 128,
+    radius: 0 - 60,
 },
 {
-    x : 2811,
-    y : 419,
-    width : 128,
-    height : 128,
+    x: 2811 - scrollingSpeed,
+    y: 419,
+    width: 128,
+    height: 128,
+    radius: 0 - 60,
 },
 ];
 
@@ -75,15 +87,19 @@ const floatingEnemyImg = new Image;
 floatingEnemyImg.src = "images/enemy.png";
 const bombImg = new Image;
 bombImg.src = "images/bomb.png";
+const playerDeathImg = new Image;
+playerDeathImg.src = "images/explosion.png";
 
 /* SELECTORS */
 const playButton = document.querySelector('#playBtn');
 const menuDiv = document.querySelector('.menu');
 const pauseDiv = document.querySelector('#pauseScreen');
 const mainMenuButton = document.querySelector('.mainMenuButton');
+const mainMenuButton2 = document.querySelector('.mainMenuButton2');
+const restartDiv = document.querySelector('#restartScreen');
 
-ctx.canvas.width  = window.innerWidth*4;                            // HERE is *2 because background is 3840px width, not 1920px
-  ctx.canvas.height = window.innerHeight - 20;
+ctx.canvas.width = window.innerWidth * 4;                            // HERE is *2 because background is 3840px width, not 1920px
+ctx.canvas.height = window.innerHeight - 20;
 
 
 console.log("canvas width = " + canvas.width + " canvas height = " + canvas.height);
@@ -105,8 +121,8 @@ function randomBall() {
 
 function floatingEnemyLogic() {
     const enemiesNr = floatingEnemy.length;
-    for (let i = 0; i < enemiesNr; i++){
-    
+    for (let i = 0; i < enemiesNr; i++) {
+
         floatingEnemy[i].x += Math.floor(Math.random() * 14 + 1);
         floatingEnemy[i].y += Math.floor(Math.random() * 14 + 1);
 
@@ -115,9 +131,13 @@ function floatingEnemyLogic() {
     }
 }
 
+function reloadPage() {
+    window.location.reload();
+}
+
 function playerHitbox() {
     ctx.beginPath();
-    ctx.arc(player.x,player.y,  player.width/2, 0, Math.PI * 2, true);
+    ctx.arc(player.x, player.y, player.width / 2, 0, Math.PI * 2, true);
     ctx.fillStyle = "pink";
     ctx.fill();
 }
@@ -129,103 +149,147 @@ function drawPlayer() {
     ctx.drawImage(playerImg, player.width * player.thisFrame, 0, player.width, player.height, player.x, player.y, player.width, player.height);
 }
 
+function playerDies() {
+    expframeTime += 6.9;
+    expframeTime = expframeTime % 710;
+    expthisFrame = Math.round(expframeTime / 10);
+    const deadX = player.x;
+    const deadY = player.y;
+    playerImg.src = "images/empty.png";
+    canIMove = false;
+    ctx.drawImage(playerDeathImg, 100 * expthisFrame, 0, 100, 100, deadX - 18 * 2, deadY - 18 * 2, 100 * 2, 100 * 2);
+    setTimeout(function () {
+        clearInterval(gameInterval);
+        restartDiv.classList.remove('hide');
+    }, 1600); // Cancel set interval adter explosion animation
+}
+
+function collision(object1, object2) {
+    let dx = object1.x - object2.x;
+    let dy = object1.y - object2.y;
+    let distance = Math.sqrt(dx * dx + dy * dy);
+
+    if (distance < object1.radius + object2.radius) {
+        playerIsDead = true; // When collied, player is dead
+        //clearInterval(gameInterval);
+    }
+}
+
 /* Main Functions init BELOW */
 // Clicking button PLAY  in menu
-playButton.addEventListener('click', function(){        
+playButton.addEventListener('click', function () {
     menuDiv.classList.add('hide');
     gameState = true;
     Game();
     console.log('gamestate = true');
 });
 
-//randomBall();
-
+/* START and body of the game itself */
 function Game() {
-if (gameState == true){
-    clearInterval(gameInterval);
-    gameInterval = setInterval(function() {
-    ctx.fillStyle = "#1B1464";
-    //ctx.clearRect(0,0,canvas.width, canvas.height);                                
-    //ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(backgroundImg,scrollingSpeed, 0,3840, canvas.height);                       // Background        
-    ctx.drawImage(backgroundImg,scrollingSpeed+ 3840 , 0,3840, canvas.height);             // Second Background 
-    ctx.drawImage(backgroundImg,scrollingSpeed+ 3840*2 , 0,3840, canvas.height);           //Third Backgorund
-    ctx.drawImage(backgroundImg,scrollingSpeed+ 3840*3 , 0,3840, canvas.height);           //Fourth Background
-    scrollingSpeed -= 2;                                                                         // Speed of scrolling the background
-    drawPlayer(); // Spawn Player
-    //playerHitbox();
-    
-    // Drawing floating blue enemy
-    for (let x = 0; x < floatingEnemy.length; x++){    
-        ctx.drawImage(floatingEnemyImg, floatingEnemy[x].x + scrollingSpeed, floatingEnemy[x].y, floatingEnemy[x].width, floatingEnemy[x].height);  
+    if (gameState == true) {
+        clearInterval(gameInterval);
+        gameInterval = setInterval(function () {
+            ctx.fillStyle = "#1B1464";
+            //ctx.clearRect(0,0,canvas.width, canvas.height);                                
+            //ctx.fillRect(0, 0, canvas.width, canvas.height);
+            ctx.drawImage(backgroundImg, scrollingSpeed, 0, 3840, canvas.height);                       // Background        
+            ctx.drawImage(backgroundImg, scrollingSpeed + 3840, 0, 3840, canvas.height);             // Second Background 
+            ctx.drawImage(backgroundImg, scrollingSpeed + 3840 * 2, 0, 3840, canvas.height);           //Third Backgorund
+            ctx.drawImage(backgroundImg, scrollingSpeed + 3840 * 3, 0, 3840, canvas.height);           //Fourth Background
+            scrollingSpeed -= 2;                                                                         // Speed of scrolling the background
+            drawPlayer(); // Spawn Player
+            //playerHitbox();
+
+            // Drawing floating blue enemy
+            for (let x = 0; x < floatingEnemy.length; x++) {
+                ctx.drawImage(floatingEnemyImg, floatingEnemy[x].x, floatingEnemy[x].y, floatingEnemy[x].width, floatingEnemy[x].height);
+                collision(player, floatingEnemy[x]);    // COLLISION player with floating enemy
+            }
+            floatingEnemyLogic();       // Apply a logic to the enemy
+
+            //ctx.drawImage(bombImg, 755 + scrollingSpeed, 466, 64, 64); // drawing random bomb ! IMPORTANT
+
+            if (canIMove == true) {
+                if ((button["w"] || button["W"]) && player.y >= 0) player.y -= player.speed;
+                if ((button["a"] || button["A"]) && player.x >= 0) player.x -= player.speed;
+                if ((button["s"] || button["S"]) && player.y <= canvas.height - player.height) player.y += player.speed;
+                if ((button["d"] || button["D"]) && player.x <= canvas.width - player.width) player.x += player.speed;
+            }
+            if (button["Escape"]) {  // On ESCAPE click go back to menu
+                clearInterval(gameInterval);
+                pauseDiv.classList.remove('hide');
+                /*menuDiv.classList.remove('hide');
+                gameState = false;*/
+            }
+
+            for (var e = 0; e < flyingBullet.length; e++) {
+                ctx.fillStyle = "red";
+                let bullets = flyingBullet[e];
+                ctx.drawImage(bulletImg, bullets.x - bullets.width / 2, bullets.y - bullets.height / 2, bullets.width, bullets.height);
+                bullets.x += 35;
+            }
+
+            if (playerIsDead == true) {
+                playerDies();
+            }
+
+            /* COLLISIONS */
+
+
+        }, 1000 / 60);
     }
-    floatingEnemyLogic();       // Apply a logic to the enemy
-
-    ctx.drawImage(bombImg, 755 + scrollingSpeed, 466, 64, 64); // drawing random bomb
-
-    if ((button["w"] || button["W"]) && player.y >= 0) player.y-= player.speed;
-    if( (button["a"] || button["A"]) && player.x >= 0) player.x-= player.speed;
-    if ((button["s"] || button["S"]) && player.y <= canvas.height - player.height) player.y+= player.speed;
-    if ((button["d"] || button["D"]) && player.x <= canvas.width - player.width) player.x+= player.speed;
-    if(button["Escape"]){  // On ESCAPE click go back to menu
-    clearInterval(gameInterval);
-        pauseDiv.classList.remove('hide');
-    /*menuDiv.classList.remove('hide');
-    gameState = false;*/
-    }
-
-    for (var e=0; e<flyingBullet.length; e++){
-        ctx.fillStyle = "red";
-        let bullets = flyingBullet[e];
-        ctx.drawImage(bulletImg, bullets.x - bullets.width/2, bullets.y - bullets.height/2, bullets.width, bullets.height); 
-        bullets.x += 35;
-    }
-
-}, 1000/60);
-}
 }
 
 
 
 // Overwrite a char + add creating bullet on Space press
-window.addEventListener("keydown", function(e){     
+window.addEventListener("keydown", function (e) {
     button[e.key] = 1;
 
     if (e.code === 'Space') {
         flyingBullet.push({
             width: 16,
             height: 10,
-            x: player.x + player.width/2 + 50,
-            y:player.y + player.height/2,
+            x: player.x + player.width / 2 + 50,
+            y: player.y + player.height / 2,
         })
-      }
-// If ENTER is pressed when the game is paused, unpause it
-    if (e.code == 'Enter'){ 
+    }
+    // If ENTER is pressed when the game is paused, unpause it
+    if (e.code == 'Enter') {
         pauseDiv.classList.add('hide');
         gameState = true;
-         Game();
+        Game();
     }
 });
 // Creating bullet on clicking in window
-window.addEventListener("click", function(e){         
+window.addEventListener("click", function (e) {
     flyingBullet.push({
         width: 16,
         height: 10,
-        x: player.x + player.width/2 + 50,
-        y:player.y + player.height/2,
+        x: player.x + player.width / 2 + 50,
+        y: player.y + player.height / 2,
     })
-  }
+}
 );
 
-window.addEventListener("keyup", function(e){
+window.addEventListener("keyup", function (e) {
     delete button[e.key];
 });
-// When paused, on press main menu display menu and turn the game from hte start
-mainMenuButton.addEventListener('click', function(){ 
+// When paused, on press main menu display menu and turn the game from hte start IT'S FROM GAME PAUSE
+mainMenuButton.addEventListener('click', function () {
     clearInterval(gameInterval);
     pauseDiv.classList.add('hide');
     menuDiv.classList.remove('hide');
+    reloadPage();
 });
+// On game over press main menu to refresh the game IT'S FROM GAME OVER SCREEN!
+mainMenuButton2.addEventListener('click', function () {
+    clearInterval(gameInterval);
+    restartDiv.classList.add('hide');
+    menuDiv.classList.remove('hide');
+    reloadPage();
+});
+
 
 
 console.log(canvas.x);
